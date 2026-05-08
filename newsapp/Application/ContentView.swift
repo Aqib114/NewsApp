c1981efa158fd67abdfc582a1cdb5ecde8adf709
Muhammad Bilal <cs21.bilal@gmail.com>
2026-05-08 21:20:38 +0500

    feat: wire app entry and root ContentView

---
+++ newsapp/Application/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  newsapp
+//
+//  Created by Muhammad Bilal on 08/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HeadlinesView()
+    }
+}
+
+#Preview {
+    ContentView()
+}

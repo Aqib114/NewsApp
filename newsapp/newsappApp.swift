815ad867c76e5d30542b954999c08eddfd8097a2
Muhammad Bilal <cs21.bilal@gmail.com>
2026-05-08 20:15:43 +0500

    Initial Commit

---
+++ newsapp/newsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsappApp.swift
+//  newsapp
+//
+//  Created by Muhammad Bilal on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct newsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
